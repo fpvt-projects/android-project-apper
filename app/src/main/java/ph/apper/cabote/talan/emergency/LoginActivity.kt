e9c40283bb36d096692b4250c721a126624eb027
Francis Paolo <francispaolo.talan@gmail.com>
2022-07-12 01:10:43 +0800

    added firebase authentication function

--- app/src/main/java/ph/apper/cabote/talan/emergency/LoginActivity.kt
+++ app/src/main/java/ph/apper/cabote/talan/emergency/LoginActivity.kt
@@ -1,11 +1,73 @@
 package ph.apper.cabote.talan.emergency
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val lgnButton = findViewById<Button>(R.id.loginBtn)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val emailInput = findViewById<EditText>(R.id.email)
+
+        //INIT FIREBASE
+        auth = FirebaseAuth.getInstance()
+
+        lgnButton.setOnClickListener{
+            when {
+                TextUtils.isEmpty(emailInput.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please Enter Email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(passwordInput.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please Enter Password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else -> {
+                    //TODO LOGIN HERE
+                    var emailValue =  emailInput.text.toString().trim()
+                    var passwordValue = passwordInput.text.toString().trim()
+
+                    auth.signInWithEmailAndPassword(emailValue, passwordValue)
+                        .addOnSuccessListener {
+                            //LOGIN SUCCESS
+                            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                        }
+                        .addOnFailureListener{
+                            //LOGIN FAILED
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+        }
+
+        fun checkUser() {
+            val firebaseUser = auth.currentUser
+            if(firebaseUser != null) {
+                //USER IS ALREADY LOGGED-IN
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+
+        }
+
     }
 }
